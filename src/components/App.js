42c8cba9d4b97b26ed903ec9b898744e19073ed9
Lucas Coelho <l.coelhooliveira@gmail.com>
2019-03-25 17:46:26 -0300

    add react-highlight-words

--- src/components/App.js
+++ src/components/App.js
@@ -11,25 +11,20 @@ class App extends Component {
     this.state = {
       searchString: '',
       recipe: this.recipes,
-      recipeSearch: []
+      recipeSearch: this.recipes
     };
   }
 
   handleChangeFilter = e => {
     let valueFilter = e.target.value.toUpperCase()
-    
+
     let recipeFilter = this.state.recipe.filter((recipe) =>
         recipe.title.toUpperCase().includes(valueFilter) || recipe.ingredients.toUpperCase().includes(valueFilter)
     )
 
     this.setState({
-      searchString: valueFilter,
+      searchString: e.target.value,
       recipeSearch: recipeFilter
-    })
-  }
-  componentWillMount = e =>{
-    this.setState({
-      recipe: this.recipes
     })
   }
 
@@ -40,9 +35,11 @@ class App extends Component {
         <div className="container mt-10">
           <div className="row">
             {
-              this.state.searchString !== '' && this.state.recipeSearch.length === 0 ? <div>Not found</div> 
-              : 
-              <RecipeItem recipe={this.state.searchString !== '' ? this.state.recipeSearch : this.state.recipe} />
+              this.state.recipeSearch.length === 0 ? <h1>No results to show.</h1> 
+              :
+              this.state.recipeSearch.map((value,i) => (
+                <RecipeItem key={i} {...value} search={this.state.searchString} />
+              ))
             }
           </div>
         </div>
--- src/components/RecipeItem.js
+++ src/components/RecipeItem.js
@@ -1,19 +1,32 @@
 import React from 'react'
+import Highlighter from 'react-highlight-words'
 
-const RecipeItem = (props) => (
-    props.recipe && props.recipe.map((value,i) => (
-        <div className="col-sm-3 mt-4" key={i}>
-            <div className="card">
-                <img className="card-img-top img-fluid" src={value.thumbnail} alt={value.title} />
-                <div className="card-body">
-                    <h5 className="card-title">{value.title}</h5>
-                    <p className="card-text">
-                        <strong>Ingredients: </strong>{value.ingredients}
-                    </p>
-                </div>
+const RecipeItem = ({title,thumbnail,ingredients,search = ''}) => (
+    <div className="col-sm-3 mt-4">
+        <div className="card">
+            <img className="card-img-top img-fluid" src={thumbnail} alt={title} />
+            <div className="card-body">
+                <h5 className="card-title">
+                    <Highlighter
+                            highlightClassName="YourHighlightClass"
+                            searchWords={[search]}
+                            autoEscape={true}
+                            textToHighlight={title}
+                        />
+                </h5>
+                
+                <p className="card-text">
+                    <strong>Ingredients: </strong>
+                    <Highlighter
+                        highlightClassName="YourHighlightClass"
+                        searchWords={[search]}
+                        autoEscape={true}
+                        textToHighlight={ingredients}
+                    />
+                </p>
             </div>
         </div>
-    ))
+    </div>
 )
 
 export default RecipeItem;
